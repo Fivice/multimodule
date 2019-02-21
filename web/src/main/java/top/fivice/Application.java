3a1a59790ed04c7d026e53519e0bcf9292f40a39
Snailoct <wubin_oct@163.com>
2019-02-21 16:54:41 +0800

    application.yml添加

--- web/src/main/java/top/fivice/Application.java
+++ web/src/main/java/top/fivice/Application.java
@@ -2,8 +2,6 @@ package top.fivice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * @author wubin
@@ -13,11 +11,7 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
  * @date 2019/1/1 22:15
  **/
 @SpringBootApplication
-public class Application extends SpringBootServletInitializer {
-
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Application.class);
-    }
+public class Application{
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
